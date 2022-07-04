5226b911c60b460723c1d7464a3d32e18f084698
emmanuelokoye <emmanuelokoye@quickcheck.ng>
2022-07-04 23:44:13 +0100

    switched db to postgres

--- ministore/settings.py
+++ ministore/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = "ministore.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "mimistore",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
