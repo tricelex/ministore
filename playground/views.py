8150cb7001d80de826b36ad0034d77fb28d9fa91
emmanuelokoye <emmanuelokoye@quickcheck.ng>
2022-07-09 20:53:09 +0100

    custom manager for taggeditem

--- playground/views.py
+++ playground/views.py
@@ -4,17 +4,16 @@ from django.db.models import Q, F, Value, Func, ExpressionWrapper, DecimalField
 from django.db.models.functions import Concat
 from django.db.models.aggregates import Count, Min
 from store.models import Product, OrderItem, Order, Customer
+from django.contrib.contenttypes.models import ContentType
+from tags.models import Tag, TaggedItem
 
 
 def sayHello(request):
-    discounted_price = ExpressionWrapper(
-        F("unit_price") * 0.8, output_field=DecimalField()
-    )
-
-    query = Product.objects.annotate(
-        discounted_price=discounted_price,
-    )
+    query_set = Product.objects.all()
+    list(query_set)
+    list(query_set)
+    query_set[4]
 
     return render(
-        request, "playground/hello.html", {"name": "Chiboy", "products": query}
+        request, "playground/hello.html", {"name": "Chiboy", "tags": list(query_set)}
     )
--- tags/models.py
+++ tags/models.py
@@ -7,8 +7,19 @@ class Tag(models.Model):
     label = models.CharField(max_length=255)
 
 
+class TaggedItemManager(models.Manager):
+    def get_tags_for(self, obj_type, obj_id):
+        content_type = ContentType.objects.get_for_model(obj_type)
+
+        return TaggedItem.objects.select_related("tags").filter(
+            content_type=content_type,
+            object_id=obj_id,
+        )
+
+
 class TaggedItem(models.Model):
     tags = models.ForeignKey(Tag, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveSmallIntegerField()
     content_object = GenericForeignKey()
+    objects = TaggedItemManager()
