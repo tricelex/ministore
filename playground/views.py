2c58bfc53654ee6ccc90093f85371f84445a983b
emmanuelokoye <emmanuelokoye@quickcheck.ng>
2022-07-10 12:13:45 +0100

    create fixtures file for models

--- playground/views.py
+++ playground/views.py
@@ -1,4 +1,5 @@
 # from django.forms import DecimalField
+from itertools import product
 from django.shortcuts import render
 from django.db.models import Q, F, Value, Func, ExpressionWrapper, DecimalField
 from django.db.models.functions import Concat
@@ -6,13 +7,21 @@ from django.db.models.aggregates import Count, Min
 from store.models import Product, OrderItem, Order, Customer
 from django.contrib.contenttypes.models import ContentType
 from tags.models import Tag, TaggedItem
+from django.db import transaction
 
 
 def sayHello(request):
-    query_set = Product.objects.all()
-    list(query_set)
-    list(query_set)
-    query_set[4]
+
+    order = Order()
+    order.customer_id = 1
+    order.save()
+
+    item = OrderItem()
+    item.order = order
+    item.product_id = 1
+    item.quantity = 1
+    item.unit_price = 10
+    item.save()
 
     return render(
         request, "playground/hello.html", {"name": "Chiboy", "tags": list(query_set)}
--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["title", "unit_price", "inventory_status"]
+    list_editable = ["unit_price"]
+    list_per_page = 10
+
+    @admin.display(ordering="inventory")
+    def inventory_status(self, product) -> str:
+        return "Low" if product.inventory < 10 else "OK"
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "membership"]
+    list_editable = ["membership"]
+    ordering = ["first_name", "last_name"]
+    list_per_page = 10
+
+
+admin.site.register(models.Collection)
